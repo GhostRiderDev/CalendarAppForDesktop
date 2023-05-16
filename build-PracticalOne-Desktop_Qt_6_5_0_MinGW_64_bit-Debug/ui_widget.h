bbad91c9c8fea443357ca416b9d934c1fe951bb8
r=cos(4θ) <olvadis2004@gmail.com>
2023-05-16 09:36:34 -0500

    se trabajo en la interfaz se elimino en frame morado, falta establecer un tamaño maximo o minimo del layout que contiene la tabla del calendario y botones que permiten controlar este

--- build-PracticalOne-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/ui_widget.h
+++ build-PracticalOne-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/ui_widget.h
@@ -14,14 +14,15 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QComboBox>
 #include <QtWidgets/QCommandLinkButton>
-#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpinBox>
-#include <QtWidgets/QSplitter>
 #include <QtWidgets/QTableView>
 #include <QtWidgets/QTableWidget>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -29,95 +30,138 @@ QT_BEGIN_NAMESPACE
 class Ui_Widget
 {
 public:
-    QFrame *frame;
-    QCommandLinkButton *crearEvento;
-    QPushButton *pushButton;
-    QPushButton *buttonExit;
-    QTableWidget *tablecalendar;
-    QSplitter *splitter;
+    QGridLayoutTableAndbuttonsut;
+    QVBoxLayout *verticalLayout_3;
+    QHBoxLayout *horizontalLayout_2;
     QPushButton *buttonAtras;
     QComboBox *mesNow;
     QLabel *label;
     QSpinBox *spinBoxAnio;
     QLabel *anio;
     QPushButton *buttonNextMonth;
+    QTableWidget *tablecalendar;
+    QVBoxLayout *verticalLayout_2;
+    QCommandLinkButton *crearEvento;
+    QPushButton *pushButton;
+    QPushButton *buttonExit;
     QTableView *TableBase;
 
     void setupUi(QWidget *Widget)
     {
         if (Widget->objectName().isEmpty())
             Widget->setObjectName("Widget");
-        Widget->resize(800, 600);
+        Widget->resize(731, 562);
         Widget->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
-        frame = new QFrame(Widget);
-        frame->setObjectName("frame");
-        frame->setGeometry(QRect(0, 0, 181, 601));
-        frame->setLayoutDirection(Qt::LeftToRight);
-        frame->setStyleSheet(QString::fromUtf8("background-color: rgb(128, 0, 128);\n"
-""));
-        frame->setFrameShape(QFrame::StyledPanel);
-        frame->setFrameShadow(QFrame::Raised);
-        crearEvento = new QCommandLinkButton(frame);
+        LayoutTableAndbuttons QGridLayout(Widget);
+        LayoutTableAndbuttonsbjectName("gridLayout");
+        LayoutTableAndbuttons = new QVBoxLayout();
+        LayoutTableAndbuttons->setObjectName("verticalLayout_3");
+        verticalLayout_3->setSizeConstraint(QLayout::SetFixedSize);
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        horizontalLayout_2->setSizeConstraint(QLayout::SetFixedSize);
+        buttonAtras = new QPushButton(Widget);
+        buttonAtras->setObjectName("buttonAtras");
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/new/prefix1/flechaRetroceder.png"), QSize(), QIcon::Normal, QIcon::Off);
+        buttonAtras->setIcon(icon);
+        buttonAtras->setIconSize(QSize(50, 50));
+        buttonAtras->setFlat(true);
+
+        horizontalLayout_2->addWidget(buttonAtras);
+
+        mesNow = new QComboBox(Widget);
+        mesNow->setObjectName("mesNow");
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(mesNow->sizePolicy().hasHeightForWidth());
+        mesNow->setSizePolicy(sizePolicy);
+
+        horizontalLayout_2->addWidget(mesNow);
+
+        label = new QLabel(Widget);
+        label->setObjectName("label");
+
+        horizontalLayout_2->addWidget(label);
+
+        spinBoxAnio = new QSpinBox(Widget);
+        spinBoxAnio->setObjectName("spinBoxAnio");
+        QSizePolicy sizePolicy1(QSizePolicy::Maximum, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(spinBoxAnio->sizePolicy().hasHeightForWidth());
+        spinBoxAnio->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_2->addWidget(spinBoxAnio);
+
+        anio = new QLabel(Widget);
+        anio->setObjectName("anio");
+
+        horizontalLayout_2->addWidget(anio);
+
+        buttonNextMonth = new QPushButton(Widget);
+        buttonNextMonth->setObjectName("buttonNextMonth");
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/new/prefix1/flechaAvanzar (1).png"), QSize(), QIcon::Normal, QIcon::Off);
+        buttonNextMonth->setIcon(icon1);
+        buttonNextMonth->setIconSize(QSize(50, 50));
+        buttonNextMonth->setFlat(true);
+
+        horizontalLayout_2->addWidget(buttonNextMonth);
+
+
+       LayoutTableAndbuttons3->addLayout(horizontalLayout_2);
+
+        tablecalendar = new QTableWidget(Widget);
+        tablecalendar->setObjectName("tablecalendar");
+        sizePolicy.setHeightForWidth(tablecalendar->sizePolicy().hasHeightForWidth());
+        tablecalendar->setSizePolicy(sizePolicy);
+
+       LayoutTableAndbuttons3->addWidget(tablecalendar);
+
+
+        gridLayout->addLayoutLayoutTableAndbuttons3, 0, 1, 1, 1);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        crearEvento = new QCommandLinkButton(Widget);
         crearEvento->setObjectName("crearEvento");
-        crearEvento->setGeometry(QRect(10, 80, 171, 51));
         crearEvento->setAutoFillBackground(false);
         crearEvento->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);\n"
 "font: 700 11pt \"Arial\";"));
-        QIcon icon;
-        icon.addFile(QString::fromUtf8(":/new/prefix1/icono2.png"), QSize(), QIcon::Normal, QIcon::Off);
-        crearEvento->setIcon(icon);
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/new/prefix1/icono2.png"), QSize(), QIcon::Normal, QIcon::Off);
+        crearEvento->setIcon(icon2);
         crearEvento->setIconSize(QSize(40, 40));
         crearEvento->setAutoDefault(false);
-        pushButton = new QPushButton(frame);
+
+        verticalLayout_2->addWidget(crearEvento);
+
+        pushButton = new QPushButton(Widget);
         pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(0, 0, 91, 41));
         pushButton->setAutoFillBackground(false);
-        QIcon icon1;
-        icon1.addFile(QString::fromUtf8(":/new/prefix1/menu.png"), QSize(), QIcon::Normal, QIcon::Off);
-        pushButton->setIcon(icon1);
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8(":/new/prefix1/menu.png"), QSize(), QIcon::Normal, QIcon::Off);
+        pushButton->setIcon(icon3);
         pushButton->setIconSize(QSize(40, 40));
         pushButton->setFlat(true);
-        buttonExit = new QPushButton(frame);
+
+        verticalLayout_2->addWidget(pushButton);
+
+        buttonExit = new QPushButton(Widget);
         buttonExit->setObjectName("buttonExit");
-        buttonExit->setGeometry(QRect(0, 580, 80, 24));
-        tablecalendar = new QTableWidget(Widget);
-        tablecalendar->setObjectName("tablecalendar");
-        tablecalendar->setGeometry(QRect(190, 80, 601, 491));
-        splitter = new QSplitter(Widget);
-        splitter->setObjectName("splitter");
-        splitter->setGeometry(QRect(190, 0, 601, 51));
-        splitter->setOrientation(Qt::Horizontal);
-        buttonAtras = new QPushButton(splitter);
-        buttonAtras->setObjectName("buttonAtras");
-        QIcon icon2;
-        icon2.addFile(QString::fromUtf8(":/new/prefix1/flechaRetroceder.png"), QSize(), QIcon::Normal, QIcon::Off);
-        buttonAtras->setIcon(icon2);
-        buttonAtras->setIconSize(QSize(50, 50));
-        buttonAtras->setFlat(true);
-        splitter->addWidget(buttonAtras);
-        mesNow = new QComboBox(splitter);
-        mesNow->setObjectName("mesNow");
-        splitter->addWidget(mesNow);
-        label = new QLabel(splitter);
-        label->setObjectName("label");
-        splitter->addWidget(label);
-        spinBoxAnio = new QSpinBox(splitter);
-        spinBoxAnio->setObjectName("spinBoxAnio");
-        splitter->addWidget(spinBoxAnio);
-        anio = new QLabel(splitter);
-        anio->setObjectName("anio");
-        splitter->addWidget(anio);
-        buttonNextMonth = new QPushButton(splitter);
-        buttonNextMonth->setObjectName("buttonNextMonth");
-        QIcon icon3;
-        icon3.addFile(QString::fromUtf8(":/new/prefix1/flechaAvanzar (1).png"), QSize(), QIcon::Normal, QIcon::Off);
-        buttonNextMonth->setIcon(icon3);
-        buttonNextMonth->setIconSize(QSize(50, 50));
-        buttonNextMonth->setFlat(true);
-        splitter->addWidget(buttonNextMonth);
+
+        verticalLayout_2->addWidget(buttonExit);
+
         TableBase = new QTableView(Widget);
         TableBase->setObjectName("TableBase");
-        TableBase->setGeometry(QRect(50, 240, 256, 192));
+
+        verticalLayout_2->addWidget(TableBase);
+
+
+        gridLayout->addLayout(verticalLayout_2, 0, 0, 1, 1);
+
 
         retranslateUi(Widget);
 
@@ -127,13 +171,13 @@ public:
     void retranslateUi(QWidget *Widget)
     {
         Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
-        crearEvento->setText(QString());
-        pushButton->setText(QCoreApplication::translate("Widget", "Menu", nullptr));
-        buttonExit->setText(QCoreApplication::translate("Widget", "Exit", nullptr));
         buttonAtras->setText(QString());
         label->setText(QCoreApplication::translate("Widget", "Mes", nullptr));
         anio->setText(QCoreApplication::translate("Widget", "A\303\261o", nullptr));
         buttonNextMonth->setText(QString());
+        crearEvento->setText(QString());
+        pushButton->setText(QCoreApplication::translate("Widget", "Menu", nullptr));
+        buttonExit->setText(QCoreApplication::translate("Widget", "Exit", nullptr));
     } // retranslateUi
 
 };
